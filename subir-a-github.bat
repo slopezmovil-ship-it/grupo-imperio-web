1bb18f20ea61268afdedb6f9bf8bb4ef2b586551
Steven Lopez <steven.lopez@flbcr.com>
2026-09-17 14:39:31 -0600

    Agregar script de subida automatica a GitHub

---
+++ subir-a-github.bat
@@ -0,0 +1,11 @@
+@echo off
+title Subiendo a GitHub - Grupo Imperio
+echo ==============================================
+echo   SUBIENDO PROYECTO A GITHUB (GRUPO IMPERIO)
+echo ==============================================
+echo.
+echo Conectando con GitHub...
+git push -u origin main
+echo.
+echo Proceso finalizado.
+pause
